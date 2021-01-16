b2ce3a841753226ce9b341b30130e98624ef616b
Xavier Sala <utrescu@gmail.com>
2021-01-16 18:48:09 +0100

    actualitzar productes

--- day1-3/shoppingcart/Program.cs
+++ day1-3/shoppingcart/Program.cs
@@ -10,8 +10,10 @@ namespace shopcart
             double transport = 2.0;
             IShoppingCart cart = new ShoppingCart(transport);
 
-            cart.AddProduct(2, new Product("Coca", 12, 0.5));
-            cart.AddProduct(1, new Product("Martell", 10.5, 0.75));
+            cart.AddProduct(2, new Product("Mantega", 6.18, 0.5));
+            cart.AddProduct(1, new Product("Llet", 0.58, 0.75));
+            cart.AddProduct(2, new Product("Bistec de vedella", 2.12, 0.5));
+            cart.AddProduct(1, new Product("Carn picada", 0.63, 1));
 
             System.Console.WriteLine($"Total: {cart.GetTotal()} euros (transport: {cart.GetTransportPrice()} euros)");
         }
