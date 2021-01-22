027230b2ccdf67b689b31f65eee155972b711ae3
Xavier Sala <utrescu@gmail.com>
2021-01-22 17:07:39 +0100

    Pantalles de login i registre

---
+++ day3-3/ElTemps/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+    sans: ['fontdinerSwanky', 'sans-serif'],
+    display: ['fontdinerSwanky', 'sans-serif'],
+    body: ['fontdinerSwanky', 'sans-serif']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
