96218989bf81ec903b1e574905aaba6a215f1651
Xavier Sala <utrescu@gmail.com>
2021-02-03 12:03:29 +0100

    Arreglar exemple de la Porta

--- day1-1/exemplePorta/Models/Porta.cs
+++ day1-1/exemplePorta/Models/Porta.cs
@@ -6,17 +6,40 @@ namespace exemplePorta.Models
 {
     class Porta
     {
+
         private bool _esOberta;
+        private bool _tancatAmbClau;
+        private bool _teClau;
+
+        public Porta(bool teClau = false)
+        {
+            TeClau = teClau;
+            _tancatAmbClau = false;
+        }
 
         public void Acciona()
         {
+            if (_tancatAmbClau)
+            {
+                return;
+            }
             _esOberta = !_esOberta;
+        }
+
+        public bool TeClau { get => _teClau; set => _teClau = value; }
+
+        public void GiraLaClau()
+        {
+            if (!TeClau)
+            {
+                return;
+            }
+            _tancatAmbClau = !_tancatAmbClau;
         }
 
         public bool EsOberta()
         {
-            // Error!
-            return false;
+            return _esOberta;
         }
     }
 }
--- day1-1/exemplePorta/Program.cs
+++ day1-1/exemplePorta/Program.cs
@@ -7,7 +7,14 @@ namespace exemplePorta
     {
         static void Main(string[] args)
         {
+            // Porta sense clau
             var porta = new Porta();
+            porta.Acciona();
+            Console.WriteLine($"Porta oberta: {porta.EsOberta()}");
+
+            // Porta amb clau
+            var portaAmbClau = new Porta(true);
+            porta.GiraLaClau();
             porta.Acciona();
             Console.WriteLine($"Porta oberta: {porta.EsOberta()}");
         }
